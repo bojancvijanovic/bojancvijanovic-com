27d93df46ff22cb421dcfc0d1e6ded14f7ead59b
bojancvijanovic <mail@bojancvijanovic.com>
2022-10-02 19:29:20 +0200

    changing the image

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,1 @@
-(() => {
-    const spinningLogo = document.getElementById('logo-spin');
-    const spin = () => {
-        const addSpinningClass = () => {
-            spinningLogo.classList.add('logo-animate');
-        }
-        const removeSpinningClass = () => {
-            spinningLogo.classList.remove('logo-animate');
-        }
-
-        if (!spinningLogo.classList.contains('logo-animate')) {
-            addSpinningClass();
-            setTimeout(removeSpinningClass, 1000)
-        }
-    }
-    spinningLogo.addEventListener('click', spin);
-})();
 
