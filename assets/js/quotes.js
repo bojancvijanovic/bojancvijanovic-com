5424d7d4841f28394e3feecc1fc88618fb30396e
Bojan <bojan@Miljanas-MacBook-Air.local>
2022-10-24 11:20:41 +0200

    use fetch for the quote generator

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -29,7 +29,22 @@ function loadDoc() {
 }
 
 
-loadDoc();
+//loadDoc();
 
 
-newQuote.addEventListener('click', loadDoc);
+newQuote.addEventListener('click', loadDocNew);
+
+function loadDocNew() {
+    fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+        .then(result => {return result.json()})
+        .then(data => { return data.quotes})
+        .then(quotes => {
+            let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+            let getAuthor = randomQuote.author;
+
+            let getQuote = randomQuote.quote;
+            currentQuote.innerHTML = `"${getQuote}"`;
+            currentAuthor.innerHTML = getAuthor;
+        })
+}
+loadDocNew();
